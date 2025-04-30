95acc850e3b03ab4d0d3b785372c7ac173c7af25
Yonder <jon.xjonx.nat@gmail.com>
2025-04-30 16:55:30 -0400

    add missing include

--- src/native_util.cpp
+++ src/native_util.cpp
@@ -1,5 +1,7 @@
 #include "native_util.h"
 
+#include <godot_cpp/core/class_db.hpp>
+
 Error memcpy_from_packed(uint8_t *p_dst, const Vector<uint8_t> p_src, const int p_count) {
 	if (p_count > p_src.size())
 		return ERR_PARAMETER_RANGE_ERROR;
